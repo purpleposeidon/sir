4fb70c95cdb570442694e1d4348c5171c66f9e08
purpleposeidon <purpleposeidon@gmail.com>
2022-07-21 15:08:59 -0700

    Nothing uses Castle.

--- sir/knights.rs
+++ sir/knights.rs
@@ -1,7 +1,7 @@
 use std::any::type_name;
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
-use crate::{Blade, Name};
+use crate::Blade;
 use crate::rt::*;
 use crate::util::{AnyDebug, Ty};
 
@@ -195,47 +195,4 @@ pub trait BodyVisitor {
     fn visit_enum(&mut self, visit: Visit<BodyEnum>) -> Result<(), Self::Err>;
     fn visit_vec(&mut self, visit: Visit<BodyVec>) -> Result<(), Self::Err>;
     fn visit_map(&mut self, visit: Visit<BodyMap>) -> Result<(), Self::Err>;
-}
-
-/// A collection of [`Sword`s](./rt/struct.Sword.html).
-pub struct Castle {
-    reg: HashMap<Ty, Sword>,
-    name2ty: HashMap<Name, Ty>,
-    constraints: Vec<Box<dyn FnMut(&mut HashMap<Ty, Sword>, &HashMap<Name, Ty>) + 'static + Send + Sync>>,
-    frozen: bool,
-}
-impl Castle {
-    pub fn reg(&self) -> &HashMap<Ty, Sword> {
-        &self.reg
-    }
-    pub fn add<B: Blade>(&mut self) {
-        self.add_orphan(
-            Ty::of::<B>(),
-            Sword::of::<B>(),
-        );
-    }
-    pub fn add_orphan(&mut self, ty: Ty, sword: Sword) {
-        self.assert_thawed();
-        self.name2ty.insert(ty.name, ty);
-        self.reg.insert(ty, sword);
-    }
-    pub fn constrain(
-        &mut self,
-        f: impl FnMut(
-            &mut HashMap<Ty, Sword>,
-            &HashMap<Name, Ty>,
-        ) + 'static + Send + Sync,
-    ) {
-        self.constraints.push(Box::new(f));
-    }
-    pub fn freeze(&mut self) {
-        self.assert_thawed();
-        self.frozen = true;
-        for c in std::mem::take(&mut self.constraints).iter_mut() {
-            c(&mut self.reg, &self.name2ty);
-        }
-    }
-    fn assert_thawed(&self) {
-        if self.frozen { panic!("registry frozen"); }
-    }
 }
